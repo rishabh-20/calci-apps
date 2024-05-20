7949002ec06a03c72fcd05513ba49fa4b89641f8
rishabh-20 <narayanrishabh2011@gmail.com>
2024-05-20 14:29:02 +0530

    Commited CalciInterface.java

--- CalciInterface.java
+++ CalciInterface.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 interface calci{
- int add(int a,int b);
+  int add(int a,int b);
+}
+
+public class calciImpl implements claci{
+    public int add(int a,int b){
+        return a+b;
+    }
+}
+
+class CalciDriver{
+    Public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        calciImpl calci=new calciImpl();
+        System.out.println(calci.add(a,b))
+    }
 }
